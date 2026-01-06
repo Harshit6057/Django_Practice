74335c09fa1d0ffcc9422118d8174dda1609cc0d
Harshit <harshitsb2615@gmail.com>
2026-01-07 00:22:05 +0530

    admin portal is improved

--- first_project/settings.py
+++ first_project/settings.py
@@ -41,10 +41,14 @@ INSTALLED_APPS = [
     "app", 
     "tailwind",
     "theme",
+    "django_browser_reload"
 ]
 
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+#NPM_BIN_PATH = r"C:\Program Files\nodejs\node.exe"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -54,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "first_project.urls"
