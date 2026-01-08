7e6158096bf14a29b2786e25e14b6dd82b2ecd9e
Harshit <harshitsb2615@gmail.com>
2026-01-09 01:15:50 +0530

    relationship models

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
-from .models import AppVariaty
+from .models import AppVariaty, AppReview, Store , AppCertificate
+
 
 
 # Register your models here.
-admin.site.register(AppVariaty)
+class AppReviewInline(admin.TabularInline):
+    model = AppReview
+    extra = 2
+    
+class AppVariatyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    search_fields = ('name', 'type')
+    list_filter = ('type', 'date_added')
+    inlines = [AppReviewInline]
+    
+    
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    search_fields = ('name', 'location'
+                     )
+    
+class AppCertificateAdmin(admin.ModelAdmin):
+    list_display = ('app', 'issued_by', 'issue_date', 'valid_until')
+    search_fields = ('app__name', 'issued_by')
+    
+admin.site.register(AppVariaty, AppVariatyAdmin)
+admin.site.register(Store, StoreAdmin )
+admin.site.register(AppCertificate, AppCertificateAdmin)
+
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 
 from django.contrib import admin
 from django.db import models
+from django.contrib.auth.models import User
 
 # Register your models here.
 class AppVariaty(models.Model):
@@ -21,3 +22,41 @@ class AppVariaty(models.Model):
   
   def __str__(self):
     return self.name
+  
+  
+#One to Many Relationship
+
+class AppReview(models.Model):
+  app = models.ForeignKey(AppVariaty, on_delete = 
+  models.CASCADE, related_name='reviews')
+  user = models.ForeignKey(User, on_delete = models.CASCADE)
+  rating = models.IntegerField()
+  comment = models.TextField()
+  date_added = models.DateTimeField(default = timezone.now)
+  
+  
+  def __str__(self):
+    return f'{self.user.username} review of {self.app.name}'
+  
+  
+# Many to Many Relationship
+
+class Store(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=200)
+  chai_varieties = models.ManyToManyField(AppVariaty, related_name = 'stores')
+  
+  def __str__(self):
+    return self.name
+
+
+# one  to one relationship
+
+class AppCertificate(models.Model):
+  app = models.OneToOneField(AppVariaty, on_delete=models.CASCADE, related_name='certificate')
+  issued_by = models.CharField(max_length=100)
+  issue_date = models.DateField()
+  valid_until = models.DateField()
+  
+  def __str__(self):
+    return f'Certificate for {self.app.name} issued by {self.issued_by}'
